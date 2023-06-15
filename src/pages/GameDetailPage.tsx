104697ba0f497098b190753055e5add66880283b
HideOnShroud <natroshviliguga5@gmail.com>
2023-06-15 23:34:58 +0400

    UI update

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, SimpleGrid, GridItem, Box } from "@chakra-ui/react";
+import { Image, Heading, Spinner, SimpleGrid, GridItem, Box } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
@@ -13,9 +13,9 @@ const GameDetailPage = () => {
     if (isLoading) return <Spinner />
     if (error || !game) throw error
     return (
-        <Box bgImg={game.background_image} bgPos='center' bgRepeat="no-repeat" backgroundSize="cover" padding={2}>
-            {/* <Image src={game.background_image}></Image> */}
-            <SimpleGrid borderRadius={20} boxShadow="dark-lg" marginTop={150} columns={{ base: 1, md: 2 }} spacing={5}>
+        <Box>
+            <Image src={game.background_image}></Image>
+            <SimpleGrid borderRadius={20} boxShadow="dark-lg" marginTop="-35vw" columns={{ base: 1, md: 2 }} padding={2} spacing={5}>
                 <GridItem borderRadius={20} bgColor='gray.800' opacity="70%" padding={2}>
                     <Heading>{game.name}</Heading>
 
